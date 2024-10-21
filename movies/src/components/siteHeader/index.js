3fe19383835116cdd9e36d1051f776ddc6d45251
Zaradin <crottyjj@hotmail.com>
2024-10-21 18:39:20 +0100

    Added upcoming movies page

--- movies/src/components/siteHeader/index.js
+++ movies/src/components/siteHeader/index.js
@@ -15,93 +15,95 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 const SiteHeader = ({ history }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const menuOptions = [
-    { label: "Home", path: "/" },
-    { label: "Favorites", path: "/movies/favorites" },
-    { label: "Option 3", path: "/" },
-    { label: "Option 4", path: "/" },
-  ];
+    const menuOptions = [
+        { label: "Home", path: "/" },
+        { label: "Favorites", path: "/movies/favorites" },
+        { label: "Upcoming", path: "/movies/upcoming" },
+        { label: "Option 4", path: "/" },
+    ];
 
-  const handleMenuSelect = (pageURL) => {
-    navigate(pageURL, { replace: true });
-  };
+    const handleMenuSelect = (pageURL) => {
+        navigate(pageURL, { replace: true });
+    };
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  return (
-    <>
-      <AppBar position="fixed" color="secondary">
-        <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
-          </Typography>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            All you ever wanted to know about Movies!
-          </Typography>
-          {isMobile ? (
-            <>
-              <IconButton
-                aria-label="menu"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={open}
-                onClose={() => setAnchorEl(null)}
-              >
-                {menuOptions.map((opt) => (
-                  <MenuItem
-                    key={opt.label}
-                    onClick={() => handleMenuSelect(opt.path)}
-                  >
-                    {opt.label}
-                  </MenuItem>
-                ))}
-              </Menu>
-            </>
-          ) : (
-            <>
-              {menuOptions.map((opt) => (
-                <Button
-                  key={opt.label}
-                  color="inherit"
-                  onClick={() => handleMenuSelect(opt.path)}
-                >
-                  {opt.label}
-                </Button>
-              ))}
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-      <Offset />
-    </>
-  );
+    return (
+        <>
+            <AppBar position="fixed" color="secondary">
+                <Toolbar>
+                    <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                        TMDB Client
+                    </Typography>
+                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                        All you ever wanted to know about Movies!
+                    </Typography>
+                    {isMobile ? (
+                        <>  
+                            <IconButton
+                                aria-label="menu"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={open}
+                                onClose={() => setAnchorEl(null)}
+                            >
+                                {menuOptions.map((opt) => (
+                                    <MenuItem
+                                        key={opt.label}
+                                        onClick={() =>
+                                            handleMenuSelect(opt.path)
+                                        }
+                                    >
+                                        {opt.label}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </>
+                    ) : (
+                        <>
+                            {menuOptions.map((opt) => (
+                                <Button
+                                    key={opt.label}
+                                    color="inherit"
+                                    onClick={() => handleMenuSelect(opt.path)}
+                                >
+                                    {opt.label}
+                                </Button>
+                            ))}
+                        </>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <Offset />
+        </>
+    );
 };
 
 export default SiteHeader;
--- movies/src/index.js
+++ movies/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import UpComingPage from "./pages/upComingPage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
@@ -12,34 +13,47 @@ import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from "./pages/addMovieReviewPage";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 360000,
-      refetchInterval: 360000,
-      refetchOnWindowFocus: false,
+    defaultOptions: {
+        queries: {
+            staleTime: 360000,
+            refetchInterval: 360000,
+            refetchOnWindowFocus: false,
+        },
     },
-  },
 });
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <SiteHeader />
-        <MoviesContextProvider>
-          <Routes>
-            <Route path="/reviews/:id" element={<MovieReviewPage />} />
-            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/movies/:id" element={<MoviePage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
-          </Routes>
-        </MoviesContextProvider>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <SiteHeader />
+                <MoviesContextProvider>
+                    <Routes>
+                        <Route
+                            path="/reviews/:id"
+                            element={<MovieReviewPage />}
+                        />
+                        <Route
+                            path="/movies/favorites"
+                            element={<FavoriteMoviesPage />}
+                        />
+                        <Route
+                            path="/movies/upcoming"
+                            element={<UpComingPage />}
+                        />
+                        <Route path="/movies/:id" element={<MoviePage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                        <Route
+                            path="/reviews/form"
+                            element={<AddMovieReviewPage />}
+                        />
+                    </Routes>
+                </MoviesContextProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 };
 
 const rootElement = createRoot(document.getElementById("root"));
