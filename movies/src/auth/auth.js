1a027f78b7cff1c9d7c27d8cc554ff03308ca27c
Zaradin <crottyjj@hotmail.com>
2024-11-08 17:08:32 +0000

    firebase auth functions

---
+++ movies/src/auth/auth.js
@@ -0,0 +1,30 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
+export const signUp = async (email, password) => {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+export const logIn = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+export const logOut = async () => {
+    try {
+        await signOut(auth);
+    } catch (error) {
+        console.error(error.message);
+    }
+};
