896ed74f333319808249467b9e555065bcd55b05
Zaradin <crottyjj@hotmail.com>
2024-11-08 15:11:56 +0000

    movie recommendations tmdb api endpoint

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -31,6 +31,21 @@ export const getMovie = (args) => {
         });
 };
 
+export const getMovieRecommendations = (movieId) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+    )
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.json().message);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
 export const getMovieCast = (args) => {
     const [, idPart] = args.queryKey;
     const { id } = idPart;
