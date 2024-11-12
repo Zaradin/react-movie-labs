a124e6f84e6e4846353564589b5fae0f75d0c957
Josh Crotty <josh@Joshs-MacBook-Pro.local>
2024-11-12 16:25:03 +0000

    fix for search bar not focusing after typing

--- movies/src/components/searchMoviesPage/index.js
+++ movies/src/components/searchMoviesPage/index.js
@@ -1,51 +1,27 @@
 import React from "react";
-import Header from "../headerMovieList";
 import MovieList from "../movieList";
 import Grid from "@mui/material/Grid2";
+import Spinner from "../spinner";
 
-function SearchMoviePage({ query, movies, action, title, handleSearchChange }) {
-    return (
-        <Grid container>
-            <Grid size={12}>
-                <Header title={title} />
-            </Grid>
-            <Grid
-                container
-                sx={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "100%",
-                }}
-            >
-                <Grid
-                    item
-                    xs={12}
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        width: "100%",
-                    }}
-                >
-                    <input
-                        type="text"
-                        value={query}
-                        onChange={handleSearchChange}
-                        placeholder="Search for movies..."
-                        style={{
-                            width: "80%",
-                            maxWidth: "900px",
-                            padding: "20px",
-                            fontSize: "25px",
-                            borderRadius: "8px",
-                            border: "1px solid",
-                        }}
-                    />
-                </Grid>
+function SearchMoviePage({ movies, loading, action }) {
+  if (loading) {
+    return <Spinner />;
+  }
 
-                <MovieList action={action} movies={movies} />
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container>
+      <Grid
+        container
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <MovieList action={action} movies={movies} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default SearchMoviePage;
